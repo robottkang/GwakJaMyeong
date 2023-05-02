9fc651b71b84e1d8f4ceada233c39a5e55534225
robottkang <robottkang39@gmail.com>
2023-05-02 22:08:34 +0900

    Feat: Create LobbyManager script
    it controls creating room and joining room

---
+++ Assets/Scripts/Realtime/Lobby/LobbyManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Lobby
+{
+    public class LobbyManager : MonoBehaviourPunCallbacks
+    {
+        [SerializeField]
+        private TMP_InputField createInput;
+        [SerializeField]
+        private TMP_InputField joinInput;
+
+
+        public void CreateRoom()
+        {
+            if (createInput.text.Length == 0)
+            {;
+                Debug.Log("no room name");
+                return;
+            }
+            PhotonNetwork.CreateRoom(createInput.text, new RoomOptions { MaxPlayers = 2 });
+        }
+
+        public void JoinRoom()
+        {
+            if (joinInput.text.Length == 0)
+            {
+                Debug.Log("no room name2");
+                return;
+            }
+            PhotonNetwork.JoinRoom(joinInput.text);
+        }
+
+        public void LeaveLobby()
+        {
+            PhotonNetwork.LeaveLobby();
+        }
+        
+
+        public override void OnJoinedRoom()
+        {
+            PhotonNetwork.LoadLevel("Room");
+        }
+
+        public override void OnJoinRoomFailed(short returnCode, string message)
+        {
+            Debug.Log("Failed to join room: " + message);
+        }
+
+        public override void OnLeftLobby()
+        {
+            PhotonNetwork.Disconnect();
+            SceneManager.LoadScene("Ready");
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            SceneManager.LoadScene("Ready");
+        }
+    }
+}
