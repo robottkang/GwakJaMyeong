110bb32f134cc24db0326809b2edcb8fc8eb7c7c
robottkang <robottkang39@gmail.com>
2023-05-10 13:27:18 +0900

    Feat: Create ReadOnlyAttribute

---
+++ Assets/Scripts/Realtime/CustomInspector/ReadOnlyAttribute.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class ReadOnlyAttribute : PropertyAttribute
+{
+    public readonly bool runtimeOnly;
+
+    public ReadOnlyAttribute(bool runtimeOnly = false)
+    {
+        this.runtimeOnly = runtimeOnly;
+    }
+}
