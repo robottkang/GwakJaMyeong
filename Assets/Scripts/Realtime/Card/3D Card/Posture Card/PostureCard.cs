0a561efb1a25d860467a193e58f872502bd21fa1
robottkang <robottkang39@gmail.com>
2023-05-10 15:46:29 +0900

    Feat: Can change Posture by PostureCard

---
+++ Assets/Scripts/Realtime/Card/3D Card/Posture Card/PostureCard.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Card
+{
+    public class PostureCard : MonoBehaviour
+    {
+        private Posture currentPosture = Posture.VomTag;
+        public Posture CurrentPosture
+        {
+            get => currentPosture;
+            set
+            {
+                switch (value)
+                {
+                    case Posture.VomTag:
+                        transform.rotation = Quaternion.Euler(0, 0, 0);
+                        break;
+                    case Posture.Pflug:
+                        transform.rotation = Quaternion.Euler(0, 45, 0);
+                        break;
+                    case Posture.Ochs:
+                        transform.rotation = Quaternion.Euler(0, 90, 0);
+                        break;
+                    case Posture.Alber:
+                        transform.rotation = Quaternion.Euler(0, 0, 180);
+                        break;
+                    default:
+                        Debug.LogError("Invalid Posture");
+                        break;
+                }
+            }
+        }
+    }
+
+    [Flags]
+    public enum Posture
+    {
+        None = 0,
+        VomTag = 1,
+        Pflug = 2,
+        Ochs = 4,
+        Alber = 8,
+    }
+}
---
+++ Assets/Scripts/Realtime/Card/3D Card/Posture Card/PostureChanger.cs
@@ -0,0 +1,95 @@
+using Card;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostureChanger : MonoBehaviour
+{
+    [Header("- Posture Objects")]
+    [SerializeField]
+    private PostureInfo[] postureInfos = new PostureInfo[4];
+
+    [Space]
+    [SerializeField]
+    private PostureCard postureCard;
+    //[HideInInspector]
+    public Posture availablePosture;
+
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+
+        Initialize();
+    }
+
+    private void OnMouseDown()
+    {
+        foreach (var postureInfo in postureInfos)
+        {
+            if (availablePosture.HasFlag(postureInfo.posture))
+                postureInfo.postureObject.SetActive(true);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit mouseRaycastHit);
+        
+        Vector3 v = mouseRaycastHit.point - transform.position;
+        const int minDistanceToChangePosture = 1;
+
+        if (v.magnitude > minDistanceToChangePosture)
+        {
+            float angle = Vector3.SignedAngle(Vector3.forward, v, Vector3.up);
+            SetPosture(angle);
+#if UNITY_EDITOR
+            Debug.Log(angle);
+#endif
+        }
+
+        foreach (var postureInfo in postureInfos)
+        {
+            if (availablePosture.HasFlag(postureInfo.posture))
+                postureInfo.postureObject.SetActive(false);
+        }
+    }
+
+    private void SetPosture(float angle)
+    {
+        foreach (var postureInfo in postureInfos)
+        {
+            if (postureInfo.startAngle < postureInfo.endAngle && postureInfo.startAngle <= angle && angle <= postureInfo.endAngle)
+            {
+                postureCard.CurrentPosture = postureInfo.posture;
+                return;
+            }
+            else if (postureInfo.startAngle > postureInfo.endAngle && (postureInfo.startAngle <= angle || angle <= postureInfo.endAngle))
+            {
+                postureCard.CurrentPosture = postureInfo.posture;
+                return;
+            }
+        }
+    }
+
+    public void Initialize()
+    {
+        foreach (var postureInfo in postureInfos)
+        {
+            postureInfo.postureObject.SetActive(false);
+        }
+    }
+
+    [System.Serializable]
+    private struct PostureInfo
+    {
+        public Posture posture;
+        public GameObject postureObject;
+        [Tooltip("The foward direction is zero degrees")]
+        [Range(-180f, 180f)]
+        public float startAngle;
+        [Range(-180f, 180f)]
+        public float endAngle;
+    }
+}
