8ddc1044c3cfc957edae8b8db0166c7bb22df9e3
robottkang <robottkang39@gmail.com>
2023-05-10 13:26:42 +0900

    Feat: Bind with DeckBuilding namespace

---
+++ Assets/Scripts/Realtime/Deck Building/Inventory.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Card;
+
+namespace DeckBuilding
+{
+    public class Inventory : MonoBehaviour, IDropHandler
+    {
+        public void OnDrop(PointerEventData eventData)
+        {
+            if (transform.childCount >= 11) return;
+
+            AppendChild(eventData.pointerDrag);
+        }
+
+        public void AppendChild(GameObject cardObject)
+        {
+            cardObject.GetComponent<DragalbeCard>().targetParent = transform;
+        }
+    }
+}
