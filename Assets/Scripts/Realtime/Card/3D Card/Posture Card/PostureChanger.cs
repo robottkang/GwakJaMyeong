d89903dd5426e95c443e7d8e876014b9f170755e
robottkang <robottkang39@gmail.com>
2023-06-01 10:01:35 +0900

    Refactor: Add Room namespace

--- Assets/Scripts/Realtime/Card/3D Card/Posture Card/PostureChanger.cs
+++ Assets/Scripts/Realtime/Card/3D Card/Posture Card/PostureChanger.cs
@@ -3,90 +3,109 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PostureChanger : MonoBehaviour
+namespace Room
 {
-    [Header("- Posture Objects")]
-    [SerializeField]
-    private PostureInfo[] postureInfos = new PostureInfo[4];
-
-    [Space]
-    [SerializeField]
-    private PostureCard postureCard;
-    //[HideInInspector]
-    public Posture availablePosture;
-
-    private Camera mainCamera;
-
-    private void Awake()
+    public class PostureChanger : MonoBehaviour
     {
-        mainCamera = Camera.main;
+        [Header("- Posture Objects")]
+        [SerializeField]
+        private PostureInfo[] postureInfos = new PostureInfo[4];
 
-        Initialize();
-    }
+        [Space]
+        [SerializeField]
+        private PostureCard postureCard;
+        //[HideInInspector]
+        public Posture availablePosture;
 
-    private void OnMouseDown()
-    {
-        foreach (var postureInfo in postureInfos)
+        private Camera mainCamera;
+
+        private void Awake()
         {
-            if (availablePosture.HasFlag(postureInfo.posture))
-                postureInfo.postureObject.SetActive(true);
-        }
-    }
-
-    private void OnMouseUp()
-    {
-        Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit mouseRaycastHit, Mathf.Infinity, LayerMask.GetMask("Board"));
-        
-        Vector3 v = mouseRaycastHit.point - transform.position;
-        const float minDistanceToChangePosture = 1f;
-
-        if (v.magnitude > minDistanceToChangePosture)
-        {
-            float angle = Vector3.SignedAngle(Vector3.forward, v, Vector3.up);
-            SetPosture(angle);
+            PageEventBus.Subscribe(Page.StrategyPlan, () =>
+            {
+                gameObject.SetActive(true);
+            });
+            PageEventBus.Subscribe(Page.Duel, () =>
+            {
+                gameObject.SetActive(false);
+            });
         }
 
-        foreach (var postureInfo in postureInfos)
+        private void Start()
         {
-            if (availablePosture.HasFlag(postureInfo.posture))
+            gameObject.SetActive(false);
+
+            mainCamera = Camera.main;
+
+            Initialize();
+        }
+
+        // display available posture objects
+        private void OnMouseDown()
+        {
+            foreach (var postureInfo in postureInfos)
+            {
+                if (availablePosture.HasFlag(postureInfo.posture))
+                    postureInfo.postureObject.SetActive(true);
+            }
+        }
+
+        // change the posture's data and transform depending on the mouse position when on mouse up
+        private void OnMouseUp()
+        {
+            Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit mouseRaycastHit, Mathf.Infinity, LayerMask.GetMask("Board"));
+
+            Vector3 v = mouseRaycastHit.point - transform.position;
+            const float minDistanceToChangePosture = 1f;
+
+            if (v.magnitude > minDistanceToChangePosture)
+            {
+                float angle = Vector3.SignedAngle(Vector3.forward, v, Vector3.up);
+                SetPosture(angle);
+            }
+
+            foreach (var postureInfo in postureInfos)
+            {
+                if (availablePosture.HasFlag(postureInfo.posture))
+                    postureInfo.postureObject.SetActive(false);
+            }
+        }
+
+        private void SetPosture(float angle)
+        {
+            foreach (var postureInfo in postureInfos)
+            {
+                if (postureInfo.startAngle < postureInfo.endAngle && postureInfo.startAngle <= angle && angle <= postureInfo.endAngle)
+                {
+                    postureCard.CurrentPosture = postureInfo.posture;
+                    return;
+                }
+                else if (postureInfo.startAngle > postureInfo.endAngle && (postureInfo.startAngle <= angle || angle <= postureInfo.endAngle))
+                {
+                    postureCard.CurrentPosture = postureInfo.posture;
+                    return;
+                }
+            }
+        }
+
+        public void Initialize()
+        {
+            foreach (var postureInfo in postureInfos)
+            {
                 postureInfo.postureObject.SetActive(false);
-        }
-    }
-
-    private void SetPosture(float angle)
-    {
-        foreach (var postureInfo in postureInfos)
-        {
-            if (postureInfo.startAngle < postureInfo.endAngle && postureInfo.startAngle <= angle && angle <= postureInfo.endAngle)
-            {
-                postureCard.CurrentPosture = postureInfo.posture;
-                return;
-            }
-            else if (postureInfo.startAngle > postureInfo.endAngle && (postureInfo.startAngle <= angle || angle <= postureInfo.endAngle))
-            {
-                postureCard.CurrentPosture = postureInfo.posture;
-                return;
             }
         }
-    }
 
-    public void Initialize()
-    {
-        foreach (var postureInfo in postureInfos)
+        [System.Serializable]
+        private struct PostureInfo
         {
-            postureInfo.postureObject.SetActive(false);
+            public Posture posture;
+            public GameObject postureObject;
+            [Tooltip("The foward direction is zero degrees")]
+            [Range(-180f, 180f)]
+            public float startAngle;
+            [Range(-180f, 180f)]
+            public float endAngle;
         }
-    }
-
-    [System.Serializable]
-    private struct PostureInfo
-    {
-        public Posture posture;
-        public GameObject postureObject;
-        [Tooltip("The foward direction is zero degrees")]
-        [Range(-180f, 180f)]
-        public float startAngle;
-        [Range(-180f, 180f)]
-        public float endAngle;
     }
 }
