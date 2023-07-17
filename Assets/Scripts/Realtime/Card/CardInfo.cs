d4e2250b08e5ca1b42e5b2145a1b0a2b3f3b1502
robottkang <robottkang@gmail.com>
2023-07-17 18:09:02 +0900

    Feat: Update CardInfo
    Add unityevnet for Card Effect

--- Assets/Scripts/Realtime/Card/CardInfo.cs
+++ Assets/Scripts/Realtime/Card/CardInfo.cs
@@ -2,40 +2,82 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Card
 {
-    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+    [CreateAssetMenu(fileName = "New CardInfo", menuName = "Card/CardInfo")]
     public class CardInfo : ScriptableObject
     {
         [SerializeField]
         protected Sprite cardSprite;
-        [SerializeField]
-        protected int damage;
+
         [SerializeField]
         protected AttackType attack;
         [SerializeField]
         protected AttackType guardPoint;
         [SerializeField]
-        protected Posture useConditions;
+        protected int damage;
         [SerializeField]
-        protected Posture postAttackPosture;
+        protected Posture requiredPosture;
+        [SerializeField]
+        protected Posture finishingPosture;
+        protected bool canBeInvaild;
 
         public string CardName => cardSprite.name;
         public Sprite CardSprite => cardSprite;
         public int Damage => damage;
         public AttackType Attack => attack;
         public AttackType GuardPoint => guardPoint;
-        public Posture UseConditions => useConditions;
-        public Posture PostAttackPosture => postAttackPosture;
+        public bool CanBeInvaild => canBeInvaild;
 
+        public UnityEvent OnCardOpen { get; set; } = new();
+        public UnityEvent OnCardTurn { get; set; } = new();
+        public UnityEvent OnCardSum { get; set; } = new();
+        public UnityEvent OnCardEffect { get; set; } = new();
+
+        [Flags]
         public enum AttackType
         {
-            none,
-            cuttingDown,
-            cuttingUp,
-            cuttingSideways,
-            poking
+            none = 0,
+            /// <summary>
+            /// 내려베기
+            /// </summary>
+            uberhauw = 1,
+            /// <summary>
+            /// 올려베기
+            /// </summary>
+            unterhau = 2,
+            /// <summary>
+            /// 횡베기
+            /// </summary>
+            horizontal = 4,
+            /// <summary>
+            /// 찌르기
+            /// </summary>
+            stechen = 8,
+        }
+
+        [Flags]
+        public enum Posture
+        {
+            None = 0,
+            /// <summary>
+            /// 세로
+            /// </summary>
+            VomTag = 1,
+            /// <summary>
+            /// 대각선
+            /// </summary>
+            Pflug = 2,
+            /// <summary>
+            /// 가로
+            /// </summary>
+            Ochs = 4,
+            /// <summary>
+            /// 뒤
+            /// </summary>
+            Alber = 8,
         }
     }
 }
--- Assets/Scripts/Realtime/Room/Field/Duel/DuelManager.cs
+++ Assets/Scripts/Realtime/Room/Field/Duel/DuelManager.cs
@@ -2,57 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
-using System.IO;
-using System;
-using IronPython.Hosting;
 
 namespace Room
 {
     public class DuelManager : MonoBehaviour
     {
-        private List<string> libs;
-        private string code;
+        public static DuelManager Instance { get; private set; }
+
 
         private void Awake()
         {
-            var url = Application.dataPath + "/Scripts/Realtime/Room/Field/Duel/comp_bind.py";
-            code = File.ReadAllText(url);
-            Debug.Log(code);
-            
-            libs = new()
-            {
-                Application.dataPath + "/ThridParty/Python/Lib"
-            };
+            Instance = GetComponent<DuelManager>();
         }
 
-        [EasyButtons.Button]
-        private void SumFirst()
-        {
-            var engine = Python.CreateEngine();
-            var scope = engine.CreateScope();
-
-            var searchPaths = engine.GetSearchPaths();
-            foreach (var lib in libs)
-            {
-                searchPaths.Add(lib);
-            }
-            engine.SetSearchPaths(searchPaths);
-
-            UniTask.RunOnThreadPool(() =>
-            {
-                var source = engine.CreateScriptSourceFromString(code);
-                source.Execute(scope);
-                
-                var func = scope.GetVariable<Func<object>>("comper");
-            }, false).Forget();
-        }
-        
-        private void SumSecond()
-        {
-
-        }
-
-        private void SumThird()
+        public static void Invaildate(bool condition)
         {
 
         }
