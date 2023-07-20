2d65e0b810cc79567115e3a8f4b7dde977cea3ce
robottkang <robottkang@gmail.com>
2023-07-20 16:14:43 +0900

    Chore: Update Inventory incompletely
    정렬 기능을 구현 중에 있음

--- Assets/Scripts/Realtime/Deck Building/Inventory.cs
+++ Assets/Scripts/Realtime/Deck Building/Inventory.cs
@@ -8,16 +8,38 @@ namespace DeckBuilding
 {
     public class Inventory : MonoBehaviour, IDropHandler
     {
+        [SerializeField]
+        private RectOffset padding;
+        [SerializeField]
+        private Vector2 spacing;
+
+        private List<GameObject> content = new();
+
         public void OnDrop(PointerEventData eventData)
         {
             if (transform.childCount >= 11) return;
 
-            AppendChild(eventData.pointerDrag);
+            AddToInventory(eventData.pointerDrag);
         }
 
-        public void AppendChild(GameObject cardObject)
+        private void AddToInventory(GameObject card)
         {
-            cardObject.GetComponent<DragalbeCard>().targetParent = transform;
+            card.transform.SetParent(transform);
+
+            content.Add(card);
+            SortChildren();
+        }
+
+        private void SortChildren()
+        {
+            content.Sort((x, y) => string.Compare(x.name, y.name));
+
+            // 자식 순서를 정렬된 리스트 순서로 업데이트
+            for (int i = 0; i < content.Count; i++)
+            {
+                content[i].transform.SetSiblingIndex(i);
+            }
+
         }
     }
 }
