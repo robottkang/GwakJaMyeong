640dca187dce2f0c39ea4c4beb27c7fe1b6d62f8
robottkang <robottkang@gmail.com>
2023-07-31 11:10:00 +0900

    Feat: Don't use PlayerView, Player Prefab

--- Assets/Scripts/Realtime/Room/Managers/GameManager.cs
+++ Assets/Scripts/Realtime/Room/Managers/GameManager.cs
@@ -4,44 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
-using System.Linq;
 
 namespace Room
 {
     public class GameManager : MonoBehaviourPunCallbacks
     {
         public static GameManager Instance { get; private set; }
-        
-        [SerializeField]
-        private GameObject counterField;
-        
-        [field:SerializeField]public StrategyPlan[] StrategyPlans { get; private set; } = new StrategyPlan[3];
-        public PlayerView MyPlayerView { get; private set; }
-        public PlayerView OpponentPlayerView { get; private set; }
 
 
         private void Awake()
         {
             Instance = this;
-            
-            MyPlayerView = PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity).GetComponent<PlayerView>();
         }
 
         private void Start()
         {
             Debug.Log(PhotonNetwork.CurrentRoom.ToString());
-        }
-
-        private void Update()
-        {
-            if (OpponentPlayerView == null)
-            {
-                foreach (var playerView in FindObjectsOfType<PlayerView>())
-                {
-                    if (playerView == MyPlayerView) return;
-                    OpponentPlayerView = playerView;
-                }
-            }
         }
 
         public void LeaveRoom()
@@ -62,9 +40,7 @@ namespace Room
 
         public override void OnPlayerLeftRoom(Player otherPlayer)
         {
-            OpponentPlayerView = null;
-
-            if (PhaseManager.Instance.CurrentPhase != Phase.WaitPlayer)
+            if (PhaseManager.CurrentPhase != Phase.BeforeStart)
             {
                 LeaveRoom();
             }
