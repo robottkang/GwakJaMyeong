631a52693f23ac7f9708fa5fd1c91615debd721d
robottkang <robottkang@gmail.com>
2023-07-31 12:26:38 +0900

    Feat: Update DuelManager
    Control StrategyPlanOrder, ActionToken

--- Assets/Scripts/Realtime/Room/Field/Duel/DuelManager.cs
+++ Assets/Scripts/Realtime/Room/Field/Duel/DuelManager.cs
@@ -1,22 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Cysharp.Threading.Tasks;
+using UnityEngine.Events;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+using Photon.Pun;
 
 namespace Room
 {
-    public class DuelManager : MonoBehaviour
+    public class DuelManager : MonoBehaviour, IOnEventCallback
     {
-        public static DuelManager Instance { get; private set; }
-        public bool setPosture = false;
-        public bool hasActionToken = false;
-        public int StrategyPlanOrder = 0;
-        public Card.PlanCardInfo currentPlanCard;
-
+        public static int StrategyPlanOrder { get; private set; }
+        public static bool HasActionToken { get; private set; } = false;
+        private static UnityEvent onSumStart = new();
 
         private void Awake()
         {
-            Instance = GetComponent<DuelManager>();
+            PhotonNetwork.AddCallbackTarget(this);
         }
+
+        private void OnDestroy()
+        {
+            PhotonNetwork.RemoveCallbackTarget(this);
+        }
+
+        public static void ResetStrategyPlanOrder()
+        {
+            if (HasActionToken)
+                StrategyPlanOrder = -1;
+            else
+                StrategyPlanOrder = 0;
+        }
+
+        public static void SubscribeOnSumStart(UnityAction action)
+        {
+            onSumStart.AddListener(action);
+        }
+
+        public static void SwapActionToken()
+        {
+            // opponent
+            PhotonNetwork.RaiseEvent((byte)DuelEventCode.SetActionToken, 
+                HasActionToken, 
+                new RaiseEventOptions { Receivers = ReceiverGroup.Others }, 
+                SendOptions.SendReliable);
+            // me
+            HasActionToken = !HasActionToken;
+        }
+
+        public void OnEvent(EventData photonEvent)
+        {
+            if (photonEvent.Code == (byte)DuelEventCode.SetActionToken)
+            {
+                HasActionToken = (bool)photonEvent.CustomData;
+            }
+        }
+    }
+
+    public enum DuelEventCode
+    {
+        Ready,
+        SetActionToken,
+        SendCardInfos,
+        SendMyPosture,
+        SendOpponentPosture,
+        SendCardDepolyment,
+        TakeDamage,
     }
 }
